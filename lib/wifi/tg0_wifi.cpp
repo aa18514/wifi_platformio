9a07c122a72193dfa791c1ae4de2404148793314
glucee@hotmail.com <glucee@hotmail.com>
2018-07-24 12:56:06 +0100

    reconnect same devices to client as many times as you want

--- lib/wifi/tg0_wifi.cpp
+++ lib/wifi/tg0_wifi.cpp
@@ -1,7 +1,4 @@
 #include "tg0_wifi.h"
-#include <list>
-#include <vector>
-using namespace std;
 
 // Replace these with your WiFi network settings
 char* ssid = "ESP8266"; //replace this with your WiFi network name
@@ -14,14 +11,18 @@ IPAddress subnet(255, 255, 255, 0);
 WiFiServer tg0_server(DATA_PORT);
 WiFiClient* tg0_clients[MAX_CLIENTS] = { NULL };
 byte buff[200] = {0};
+const boolean is_ascii = true;
 
 void setup_server(char* ssid = ssid, char* password = password)
 {/* You can remove the password parameter if you want the AP to be open. */
-  
-  WiFi.mode(WIFI_OFF);
-  WiFi.persistent(false);
+  //WiFi.mode(WIFI_OFF);
   WiFi.mode(WIFI_AP);
   boolean result = WiFi.softAP(ssid, password, channel);
+  //while(!result) {
+    //Serial.println("trying to connect!");
+    //  result = WiFi.softAP(ssid, password, channel);
+  //}
+  Serial.println(result);
   if(result==true) {
     Serial.println("wifi established!");
     if (ip_addr.fromString(server_address) && gateway.fromString(gateway_address)) {
@@ -34,36 +35,33 @@ void setup_server(char* ssid = ssid, char* password = password)
 
 void check_client() {
   WiFiClient client = tg0_server.available();
-  if (client != NULL) {
-    Serial.println(client.remoteIP());
+  if (client) {
   
     for (int i = 0; i < MAX_CLIENTS; i++) {
-      if (tg0_clients[i] != NULL && tg0_clients[i]->remoteIP() == client.remoteIP()) {
-        tg0_clients[i]->stop();
-        delete tg0_clients[i];
-        tg0_clients[i] = new WiFiClient(client);
-        break;
+        if (tg0_clients[i] != NULL && tg0_clients[i]->remoteIP() == client.remoteIP()) {
+          tg0_clients[i]->stop();
+          delete tg0_clients[i];
+          tg0_clients[i] = new WiFiClient(client);
+          break;
       }
-      else if (tg0_clients[i] == NULL) {
-        Serial.print(" client number: "); Serial.print(i);
+      if (tg0_clients[i] == NULL) {
         tg0_clients[i] = new WiFiClient(client); 
         tg0_clients[i]->setTimeout(10);
         tg0_clients[i]->setNoDelay(true);
+        Serial.print(tg0_clients[i]->remoteIP());
         break;
       }
     }
   }
   for (int i = 0; i < MAX_CLIENTS; i++) {
-    Serial.print(tg0_clients[i]->connected());
-    if (tg0_clients[i] != NULL && tg0_clients[i]->available() > 0) {
+    if (tg0_clients[i] != NULL){
       int bytes_to_read = tg0_clients[i]->available();
-      int returned_bytes = tg0_clients[i]->readBytes(buff, bytes_to_read);
-      if (returned_bytes > 0) {
-        Serial.print(" returned bytes "); Serial.println(returned_bytes);
-        //Serial.write(buff, returned_bytes);
-      } else {
-       // Serial.println("this client is NULL!");
+      if (tg0_clients[i] != NULL && bytes_to_read > 0) {
+        int returned_bytes = tg0_clients[i]->readBytes(buff, bytes_to_read);
+        if (returned_bytes > 0) {
+          Serial.write(buff, returned_bytes);
+        } 
       }
-    }
-  } 
+    } 
+  }
 }
